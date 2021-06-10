ddef21071e930ec9b3a5668be57f34683c94b954
joannsiciliano <joannsiciliano@gmail.com>
2021-06-10 10:50:35 -0600

    users table added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create, :login]
+
+  def index
+    @users=User.all
+    render json: @users
+  end
+
+  def profile
+    render json: @user
+  end
+
+  def create
+    @user=User.create(user_params)
+    # @user.valid?(render json: @user, status: :created : render json: @user, status: :not_acceptable)
+  end
+
+  def login
+  @user=User.find_by(username: params[:user][:username])
+
+    if @user&&@user.authenticate(params[:user][:password])
+      @token=JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
+      render json: {user: @user, token: @token}
+    else
+      render json: {error: "Invalid Credentials"}, status: :unauthorized
+  end
+end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password,)
+  end
+end
