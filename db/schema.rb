92c5131b60c30836df7e6a6b5a4b0976809affb5
joannsiciliano <joannsiciliano@gmail.com>
2021-06-16 10:20:10 -0600

    message table created and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_174639) do
+ActiveRecord::Schema.define(version: 2021_06_16_161717) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "moods", force: :cascade do |t|
     t.string "feeling"
