d021752588a185389186fb3d1d290b2104cb2303
joannsiciliano <joannsiciliano@gmail.com>
2021-06-23 12:35:51 -0600

    uncomment action cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://this-is-internet.herokuapp.com/cable'
+  # config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://this-is-internet.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'https://this-is-internet.herokuapp.com', /http:\/\/this-is-internet.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
