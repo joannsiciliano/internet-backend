1f0d4a7c61430f405d4596b415f00d06d875a5f8
joannsiciliano <joannsiciliano@gmail.com>
2021-06-23 10:03:16 -0600

    heroku deployed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
     if auth_header
       @token=auth_header.split(" ")[1]
       begin
-        @user_id=JWT.decode(@token,Rails.application.secrets.secret_key_base[0])[0]["user_id"]
+        @user_id=JWT.decode(@token, ENV["SECRET_KEY_BASE"])[0]["user_id"]
   
       rescue JWT::DecodeError
         nil
