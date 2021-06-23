4f3ef5d7e1ab4a2eebb68d387ff1aa33e801a071
joannsiciliano <joannsiciliano@gmail.com>
2021-06-23 11:53:24 -0600

    troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://this-is-internet.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://this-is-internet.herokuapp.com', /http:\/\/this-is-internet.herokuapp.com.*/ ]
+  # config.action_cable.url = 'wss://this-is-internet.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'https://this-is-internet.herokuapp.com', /http:\/\/this-is-internet.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
